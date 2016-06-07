8f4acf86f2d14673526016303fb8c7b837e1be1c
Dinmukhambet <dimashdim@gmail.com>
2016-06-07 15:49:33 +0600

    7 из 7

---
+++ src/FindProductClass.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+/**
+ * Created by Admin on 07.06.2016.
+ */
+public class FindProductClass {
+    public static void main(String[] args) {
+        int answer=1;
+        Scanner sc = new Scanner(System.in);
+        int n=sc.nextInt();
+        int [] array= new int[n];
+        for (int i=0;i<array.length;i++){
+            array[i]=sc.nextInt();
+        }
+        for (int i=0;i<array.length;i++){
+            answer= (int) (answer*array[i]%(Math.pow(10,9)+7));
+        }
+        System.out.println(answer);
+    }
+}

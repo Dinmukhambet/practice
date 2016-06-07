d0b813d833677a0ec98fdda1432b55674b4f577f
Dinmukhambet <dimashdim@gmail.com>
2016-06-07 14:47:53 +0600

    псевдокод

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,20 @@
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
-    }
+   int []array=new int[5];
+        Scanner scanner = new Scanner(System.in);
+       for(int i=0;i<array.length;i++){
+           array[i]=scanner.nextInt();
+
+       }
+        for(int i=0;i<array.length;i++){
+            if (array[i]==42){
+                break;
+            }
+            System.out.println(array[i]);
+
+        }
+}
 }
